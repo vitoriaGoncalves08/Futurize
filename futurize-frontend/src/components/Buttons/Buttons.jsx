401dc592bc4464e9a6f3b4b0ecfad5090315e130
Vitória Gonçalves Passos <56117556+vitoriaGoncalves08@users.noreply.github.com>
2023-08-31 11:53:54 -0300

    add: começando com as cores

--- futurize-frontend/src/components/Buttons/Buttons.jsx
+++ futurize-frontend/src/components/Buttons/Buttons.jsx
@@ -4,47 +4,17 @@ import './Buttons.css';
 
 export default function Buttons(props) {
   return (
-    <Button 
-      variant={props.variant || "contained"}
-      startIcon={props.startIcon}
-      endIcon={props.endIcon}
-      size={props.size || "medium"}
-      color={props.color}
-      text={props.text}
-      className={props.size || "c-button"}
-      href={props.href}
-      onClick={props.onClick}>
-      {props.children}
-    </Button>
+      <Button 
+        variant={props.variant || "contained"}
+        startIcon={props.startIcon}
+        endIcon={props.endIcon}
+        size={props.size || "medium"}
+        color={props.color}
+        text={props.text}
+        className={props.size || "c-button"}
+        href={props.href}
+        onClick={props.onClick}>
+        {props.children}
+      </Button>
   );
 } 
-
-// import React from 'react';
-// import Button from '@mui/material/Button';
-// import IconButton from '@mui/material/IconButton';
-// import CircularProgress from '@mui/material/CircularProgress';
-
-// export default function Buttons({
-//   variant = 'contained',
-//   color = 'primary',
-//   children,
-//   startIcon,
-//   endIcon,
-//   loading = false,
-//   ...restProps
-// }) {
-//   const ButtonComponent = variant === 'icon' ? IconButton : Button;
-
-//   return (
-//     <ButtonComponent
-//       variant={variant === 'icon' ? undefined : variant}
-//       color={color}
-//       disabled={loading}
-//       {...restProps}
-//     >
-//       {startIcon && !loading && startIcon}
-//       {loading ? <CircularProgress size={24} /> : children}
-//       {endIcon && !loading && endIcon}
-//     </ButtonComponent>
-//   );
-// }
