1fbfba49c4276b869057d420e0a9e5565e77a0d4
Gabriel Sena <gsdcdj@gmail.com>
2023-10-12 20:55:33 -0300

    adicionando header tela inicial

---
+++ futurize-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+<div class="Header">
+        <div class="Logo">FURURIZE</div>
+        <img class="ImgPessoa" src="https://via.placeholder.com/40x40" />
+        <div class="OpcoesIcon">
+            <i class="ph ph-dots-three-vertical" id="op"></i>
+        </div>
+    </div>
