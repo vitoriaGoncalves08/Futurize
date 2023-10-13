f14ca824065092dcc758e2ae71820004d739bdba
Gabriel Sena <gsdcdj@gmail.com>
2023-10-12 21:53:31 -0300

    puxando na home

--- futurize-frontend/src/pages/home.jsx
+++ futurize-frontend/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import Buttons from "../components/Buttons/Buttons";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
+import Header from '../components/Header/Header';
 
 export default function Home() {
   const { signout } = useAuth();
