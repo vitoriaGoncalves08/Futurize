7f01572309923a426391c52b798ffeb5068a54f9
Gabriel Sena <gsdcdj@gmail.com>
2023-10-12 21:45:21 -0300

    arrumando header

--- futurize-frontend/src/components/Header/Header.jsx
+++ futurize-frontend/src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
-import "./Header.css";
+import styles from './Header.module.css'
 
-<div class="Header">
-        <div class="Logo">FURURIZE</div>
-        <img class="ImgPessoa" src="https://via.placeholder.com/40x40" />
-        <div class="OpcoesIcon">
-            <i class="ph ph-dots-three-vertical" id="op"></i>
+function Header() {
+    return (
+        <div className={styles.Header}>
+            <div className={styles.Logo}>FURURIZE</div>
+            <img className={styles.ImgPessoa} src="https://via.placeholder.com/40x40" />
+            <div className={styles.OpcoesIcon}>
+                <i class="ph ph-dots-three-vertical" id="op"></i>
+            </div>
         </div>
-    </div>
+    );
+}
+
+export default Header;
