a538bd1e32a6471a3df982d1fc3687070b56e171
Vitória G <VitoriaGoncalvesPassos@hotmail.com>
2023-10-17 22:52:38 -0300

    :fire: melhorando menu e logout (falta a table)

--- futurize-frontend/src/components/Header/Header.jsx
+++ futurize-frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 function Header() {
     return (
         <div className={styles.Header}>
-            <div className={styles.Logo}>FURURIZE</div>
+            <div className={styles.Logo}>FUTURIZE</div>
             <img className={styles.ImgPessoa} src="https://via.placeholder.com/40x40" />
             <div className={styles.OpcoesIcon}>
                 <div className={styles.op}>
--- futurize-frontend/src/components/SideBar/SideBar.jsx
+++ futurize-frontend/src/components/SideBar/SideBar.jsx
@@ -1,50 +1,128 @@
 import './SideBar.css';
+import FolderIcon from '@mui/icons-material/Folder';
+import ViewKanbanIcon from '@mui/icons-material/ViewKanban';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MenuIcon from '@mui/icons-material/Menu';
+import LogoutIcon from '@mui/icons-material/Logout';
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-function SideBar(){
+
+const Sidebar = ({children}) => {
+    const { signout } = useAuth();
+    const navigate = useNavigate();
+  
+    const handleLogout = () => {
+      signout();
+      navigate("/");
+    };
+
+    const[isOpen ,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen (!isOpen);
+    const menuItem=[
+        {
+            path:"/Projeto",
+            name:"Projeto",
+            icon:<FolderIcon/>
+        },
+        {
+            path:"/Kanban",
+            name:"Kanban",
+            icon:<ViewKanbanIcon/>
+        },
+        {
+            path:"/Calendario",
+            name:"Calendário",
+            icon:<CalendarMonthIcon/>
+        },
+        {
+            path:"/Dashboard",
+            name:"Dashboard",
+            icon:<DashboardIcon/>
+        },
+        {
+            path:"/Configuracoes",
+            name:"Configurações",
+            icon:<SettingsIcon/>
+        },
+    ]
     return (
-        <div className="rectangle12">
-            <div className="SideBar">
-                {/*<i className="ph ph-list" id="btn"></i>*/}
-                <ul className="NavList>">
-                    <li>
-                        <a href="#">
-                            {/*<i className="ph ph-folder-open"></i>*/}
-                            <span className="LinksName">Projeto</span>
-                        </a>
-                        <span className="Tooltip">Projeto</span>
-                    </li>
-                    <li>
-                        <a href="#">
-                            {/* <i className="ph ph-kanban"></i> */}
-                            <span className="LinksName">Kanban</span>
-                        </a>
-                        <span className="Tooltip">Kanban</span>
-                    </li>
-                    <li>
-                        <a href="#">
-                            {/* <i className="ph ph-calendar"></i> */}
-                            <span className="LinksName">Calendário</span>
-                        </a>
-                        <span className="Tooltip">Calendário</span>
-                    </li>
-                    <li>
-                        <a href="#">
-                            {/* <i className="ph ph-chart-bar"></i> */}
-                            <span className="LinksName">Dashboard</span>
-                        </a>
-                        <span className="Tooltip">Dashboard</span>
-                    </li>
-                    <li>
-                        <a href="#">
-                            {/* <i className="ph ph-gear"></i> */}
-                            <span className="LinksName">Configurações</span>
-                        </a>
-                        <span className="Tooltip">Configurações</span>
-                    </li>
-                </ul>
-            </div>
+        <div className="menu-lateral">
+           <div style={{width: isOpen ? "220px" : "55px"}} className="sidebar-menu">
+               <div className="top_section-menu">
+                   {/* <h3 style={{display: isOpen ? "block" : "none"}} className="logo-menu">Logo</h3> */}
+                   <div style={{marginLeft: isOpen ? "0px" : "0px"}} className="bars-menu">
+                       <MenuIcon onClick={toggle}/>
+                   </div>
+               </div>
+               {
+                   menuItem.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link-menu" activeclassName="active-menu">
+                           <div className="icon-menu">{item.icon}</div>
+                           <div style={{display: isOpen ? "block" : "none"}} className="link_text-menu">{item.name}</div>
+                       </NavLink>
+                   ))
+               }
+                <NavLink className="link-menu logout" activeclassName="active-menu logout">
+                    <div className="icon-menu logout"><LogoutIcon/></div>
+                    <div style={{display: isOpen ? "block" : "none", marginLeft: 40, marginBottom: 6}} className="link_text-menu logout" onClick={handleLogout}>Sair</div>
+                </NavLink>
+           </div>
+           <main>{children}</main>
         </div>
     );
-}
+};
 
-export default SideBar;
+export default Sidebar;
+
+
+// return (
+//     <div className="rectangle12">
+//         <div className="SideBar">
+//             {/*<i className="ph ph-list" id="btn"></i>*/}
+//             <ul className="NavList>">
+//                 <li>
+//                     <a href="#">
+//                         {/*<i className="ph ph-folder-open"></i>*/}
+//                         <span className="LinksName">Projeto</span>
+//                     </a>
+//                     <span className="Tooltip">Projeto</span>
+//                 </li>
+//                 <li>
+//                     <a href="#">
+//                         {/* <i className="ph ph-kanban"></i> */}
+//                         <span className="LinksName">Kanban</span>
+//                     </a>
+//                     <span className="Tooltip">Kanban</span>
+//                 </li>
+//                 <li>
+//                     <a href="#">
+//                         {/* <i className="ph ph-calendar"></i> */}
+//                         <span className="LinksName">Calendário</span>
+//                     </a>
+//                     <span className="Tooltip">Calendário</span>
+//                 </li>
+//                 <li>
+//                     <a href="#">
+//                         {/* <i className="ph ph-chart-bar"></i> */}
+//                         <span className="LinksName">Dashboard</span>
+//                     </a>
+//                     <span className="Tooltip">Dashboard</span>
+//                 </li>
+//                 <li>
+//                     <a href="#">
+//                         {/* <i className="ph ph-gear"></i> */}
+//                         <span className="LinksName">Configurações</span>
+//                     </a>
+//                     <span className="Tooltip">Configurações</span>
+//                 </li>
+//             </ul>
+//         </div>
+//     </div>
+// );
+// }
+// export default SideBar;
