f066edb98e43e577d77af64f75993f77cf96d8c6
Gabriel Sena <gsdcdj@gmail.com>
2023-10-24 11:00:36 -0300

    estilizando kanban

--- futurize-frontend/src/components/HeaderKanban/HeaderKanban.jsx
+++ futurize-frontend/src/components/HeaderKanban/HeaderKanban.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './HeaderKanban.css';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Avatar from '@mui/material/Avatar';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
 
 
 export default function HeaderKanban() {
   return (
     <div className='container-header-kanban'>
       <h3 className='nome-header-kanban'>NOME PROJETO</h3>
+      <StarBorderRoundedIcon></StarBorderRoundedIcon>
+      <DeleteRoundedIcon></DeleteRoundedIcon>
       <div className="integrantes-header-kanban">
         <Avatar>H</Avatar>
         <AddCircleIcon/>
