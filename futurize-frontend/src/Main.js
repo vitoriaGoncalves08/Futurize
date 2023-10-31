733b3da9f492d801190f5a39dc9cc961970adec8
Felipe Sadrak dos Santos <felipesadrak2@gmail.com>
2023-10-31 10:59:12 -0300

    Transição do Menu para landing Page
    Criação da estrutura inicial da landing page da tela de inicio

---
+++ futurize-frontend/src/Main.js
@@ -0,0 +1,54 @@
+function Main() {
+    return (
+        <div className='main'>
+        <div className='text-content'>
+        <h1> Desperte o potencial máximo dos seus projetos</h1>
+          <p>
+          Transforme o jeito de gerenciar projetos! Visualize e organize tarefas de forma 
+          intuitiva, com atualizações em tempo real e colaboração eficaz em equipe. 
+          Personalize conforme sua necessidade e alcance o sucesso com facilidade.
+          Experimente a revolução Kanban hoje! 
+          </p>
+        </div>
+
+        <div className='text-content'>
+        <h1>Personalize o seu fluxo de trabalho e ajuste conforme a necessidade do seu projeto!</h1>
+          <p>
+          O Kanban é como um quadro de tarefas virtual, onde você visualiza e move suas 
+          atividades de uma etapa para outra. Imagine post-its digitais que
+          representam suas tarefas. Arraste e solte para mudar o status:
+          de "A fazer" para "Em progresso" e finalmente para "Concluído".
+          Simples assim! 
+          </p>
+        </div>
+
+        <div className='text-content'>
+        <h1>organize-se e alcance o sucesso de forma rápida e divertida. </h1>
+          <p>
+          A nossa plataforma permite que você defina metas,
+          atribua tarefas e estabeleça prazos de maneira descomplicada.
+          Imagine reunir sua equipe em um ambiente digital onde todos têm
+          uma visão clara do que precisa ser feito.
+          </p>
+          <p>Além disso, você pode monitorar o progresso em tempo real, 
+            identificando gargalos e oportunidades de otimização. 
+            Colabore de maneira eficaz, comentando diretamente nas tarefas 
+            e recebendo notificações instantâneas.
+            </p>
+        </div>
+
+        <div className='text-content'>
+        <h1>A simplicidade do arrastar e soltar </h1>
+          <p>
+          Dentro da nossa plataforma, as tarefas são como peças de um 
+          quebra-cabeça que compõem o seu projeto. Elas podem ser criadas,
+          atribuídas a membros da equipe e definidas com prazos claros.
+          Imagine ter total controle sobre o que precisa ser feito, 
+          por quem e até quando.
+          </p>
+        </div>
+        </div>
+    )
+}
+
+export default Main;
---
+++ futurize-frontend/src/components/About/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const About = () => {
+    return <div className="about-section-container">
+        <div className="about-section-text-container">
+            <p className="primary-subheading">About</p>
+            <h1 className="primary-heading">
+                Cooperação e eficiência
+            </h1>
+            <p className="primary-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            
+            </p>
+            <p className="primary-text">
+            It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.
+            </p>
+            <div className="about-buttons-container">
+             <button className="secondary-button">Learn More</button>   
+            
+            </div>
+        </div>
+        </div>
+
+};
