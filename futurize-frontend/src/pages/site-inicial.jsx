136c9a2498173de1b21d68c57562fa7e17564711
Gabriel Sena <gsdcdj@gmail.com>
2023-11-01 12:45:04 -0300

    :construction: arumando site-inicial

--- futurize-frontend/src/pages/site-inicial.jsx
+++ futurize-frontend/src/pages/site-inicial.jsx
@@ -1,7 +1,6 @@
 import Buttons from '../components/Buttons/Buttons';
 import '../../public/assets/css/site-inicial.css';
 import { useNavigate } from 'react-router-dom';
-import wave from '/assets/img/wave-footer.svg';
 
 function SiteInicial() {
   const navigate = useNavigate();
@@ -14,18 +13,18 @@ function SiteInicial() {
 
         <div className="MenuItens">
           <ul>
-            <li><a href="#">Como funciona?</a></li>
-            <li><a href="#">Modelos</a></li>
-            <li><a href="#">Crie seus projetos</a></li>
-            <li><a href="#">Tarefas em tempo real</a></li>
-            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#funciona">Funcionamento</a></li>
+            <li><a href="#modelo">Modelos</a></li>
+            <li><a href="#projeto">Projetos</a></li>
+            <li><a href="#tarefas">Tarefas</a></li>
+            <li><a href="#sobre">Sobre nós</a></li>
           </ul>
         </div>
         <Buttons onClick={() => navigate('/login')}>Entrar</Buttons>
       </nav>
       <div className="HeroContent">
         <div className="Titulo">
-          <p>Gerencie seus projetos</p>
+          <p>Gerencie seus projetos acadêmicos</p>
         </div>
 
         <div className="Subtitulo">
@@ -42,52 +41,42 @@ function SiteInicial() {
 
       </div>
       <div className='main-content'>
-          <div className='text-content'>
+          <div id="funciona" className='text-content'>
             <h1> Desperte o potencial máximo dos seus projetos</h1>
-             
             <p>
-              Transforme o jeito de gerenciar projetos! Visualize e organize tarefas de forma 
-              intuitiva, com atualizações em tempo real e colaboração eficaz em equipe. 
-              Personalize conforme sua necessidade e alcance o sucesso com facilidade.
-              Experimente a revolução Kanban hoje! 
+              Transforme o jeito de gerenciar projetos! Visualize e organize tarefas de forma intuitiva, com atualizações em tempo real e colaboração eficaz em equipe. Personalize conforme sua necessidade e alcance o sucesso com facilidade. Experimente a revolução Kanban hoje! 
             </p>
           </div>
 
-          <div className='text-content'>
+          <div id="modelo" className='text-content'>
             <h1>Personalize o seu fluxo de trabalho e ajuste conforme a necessidade do seu projeto!</h1>
             <p>
-              O Kanban é como um quadro de tarefas virtual, onde você visualiza e move suas 
-              atividades de uma etapa para outra. Imagine post-its digitais que
-              representam suas tarefas. Arraste e solte para mudar o status:
-              de "A fazer" para "Em progresso" e finalmente para "Concluído".
-              Simples assim!
+              O Kanban é como um quadro de tarefas virtual, onde você visualiza e move suas atividades de uma etapa para outra. Imagine post-its digitais que representam suas tarefas. Arraste e solte para mudar o status de: "A fazer" para "Em progresso" e finalmente para "Concluído". Simples assim!
             </p>
           </div>
 
-          <div className='text-content'>
+          <div id="projeto" className='text-content'>
             <h1>Organize-se e alcance o sucesso de forma rápida e divertida. </h1>
             <p>
-              A nossa plataforma permite que você defina metas,
-              atribua tarefas e estabeleça prazos de maneira descomplicada.
-              Imagine reunir sua equipe em um ambiente digital onde todos têm
-              uma visão clara do que precisa ser feito.
+              A nossa plataforma permite que você defina metas, atribua tarefas e estabeleça prazos de maneira descomplicada. Imagine reunir sua equipe em um ambiente digital onde todos têm uma visão clara do que precisa ser feito.
             </p>
             <p>
               Além disso, você pode monitorar o progresso em tempo real, identificando gargalos e oportunidades de otimização. Colabore de maneira eficaz, comentando diretamente nas tarefas e recebendo notificações instantâneas.
             </p>
           </div>
 
-          <div className='text-content'>
+          <div id="tarefas" className='text-content'>
             <h1>A simplicidade do arrastar e soltar </h1>
             <p>
-              Já imaginou ter total controle sobre o que precisa ser feito? 
-              Por quem e até quando?
-              </p>
+              Já imaginou ter total controle sobre o que precisa ser feito? Por quem e até quando?
+            </p>
             <p>
-               Dentro da nossa plataforma, as tarefas são como peças de um 
-              quebra-cabeça que compõem o seu projeto. Elas podem ser criadas,
-              atribuídas a membros da equipe e definidas com prazos claros.
-                  </p>
+              Dentro da nossa plataforma, as tarefas são como peças de um quebra-cabeça que compõem o seu projeto. Elas podem ser criadas, atribuídas a membros da equipe e definidas com prazos claros.
+            </p>
+          </div>
+          <div id="sobre" className='text-content'>
+            <h1>Sobre nós</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis animi non quis nihil suscipit pariatur, vero veniam ab tempora nobis blanditiis illum autem laborum quod exercitationem debitis laudantium. Distinctio, omnis?</p>
           </div>
         </div>
         
