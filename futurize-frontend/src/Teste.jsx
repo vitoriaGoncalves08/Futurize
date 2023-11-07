87945910707dd53a2cbd9ad7a724f48472a9d044
Gabriel Sena <gsdcdj@gmail.com>
2023-11-07 10:51:26 -0300

    arrumando os componentes

--- futurize-frontend/src/Teste.jsx
+++ futurize-frontend/src/Teste.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from '/assets/react.svg';
-import viteLogo from '/vite.svg';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Buttons from './components/Buttons/Buttons';
 import LoadingButton from '@mui/lab/LoadingButton';
