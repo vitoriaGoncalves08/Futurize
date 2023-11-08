f11798a70e1627345bdc28907bcc28b7c634e04d
Gabriel Sena <gsdcdj@gmail.com>
2023-11-08 12:44:31 -0300

    :construction: arrumando SiteInicial v2

--- futurize-frontend/src/components/SiteInicial/SiteInicial.jsx
+++ futurize-frontend/src/components/SiteInicial/SiteInicial.jsx
@@ -1,5 +1,5 @@
-import Buttons from '../components/Buttons/Buttons';
-import '../../public/assets/css/site-inicial.css';
+import Buttons from '../Buttons/Buttons';
+import './SiteInicial.css';
 import { useNavigate } from 'react-router-dom';
 import projeto from '/assets/img/projeto.svg';
 
@@ -23,6 +23,7 @@ function SiteInicial() {
         </div>
         <Buttons onClick={() => navigate('/login')}>Entrar</Buttons>
       </nav>
+      
       <div className="HeroContent">
         <div className="Titulo">
           <p>Gerencie seus projetos acadêmicos</p>
@@ -36,13 +37,13 @@ function SiteInicial() {
         </div>
 
         <div className="ButtonCriarConta">
-          {/* <button>Criar Conta</button> */}
           <Buttons onClick={() => navigate('/Cadastro')}>Criar Conta</Buttons>
         </div>
-
       </div>
-      <div className='main-content'>
-          <div id="funciona" className='secao'>
+
+      <div className='MainContent'>
+
+          <section id="funciona" className='secao'>
             <div className="texto">
               <h1> Desperte o potencial máximo dos seus projetos</h1>
               <p>
@@ -50,43 +51,51 @@ function SiteInicial() {
               </p>
             </div>
             <img src={projeto} alt="" srcset="" />
-          </div>
+          </section>
 
           <div id="modelo" className='secao'>
-            <h1>Personalize o seu fluxo de trabalho e ajuste conforme a necessidade do seu projeto!</h1>
-            <p>
-              O Kanban é como um quadro de tarefas virtual, onde você visualiza e move suas atividades de uma etapa para outra. Imagine post-its digitais que representam suas tarefas. Arraste e solte para mudar o status de: "A fazer" para "Em progresso" e finalmente para "Concluído". Simples assim!
-            </p>
+            <div className="texto">
+              <h1>Personalize o seu fluxo de trabalho e ajuste conforme a necessidade do seu projeto!</h1>
+              <p>
+                O Kanban é como um quadro de tarefas virtual, onde você visualiza e move suas atividades de uma etapa para outra. Imagine post-its digitais que representam suas tarefas. Arraste e solte para mudar o status de: "A fazer" para "Em progresso" e finalmente para "Concluído". Simples assim!
+              </p>
+            </div>
+            <img src={projeto} alt="" srcset="" />
           </div>
 
           <div id="projeto" className='secao'>
-            <h1>Organize-se e alcance o sucesso de forma rápida e divertida. </h1>
-            <p>
-              A nossa plataforma permite que você defina metas, atribua tarefas e estabeleça prazos de maneira descomplicada. Imagine reunir sua equipe em um ambiente digital onde todos têm uma visão clara do que precisa ser feito.
-            </p>
-            <p>
-              Além disso, você pode monitorar o progresso em tempo real, identificando gargalos e oportunidades de otimização. Colabore de maneira eficaz, comentando diretamente nas tarefas e recebendo notificações instantâneas.
-            </p>
+            <div className="texto">
+              <h1>Organize-se e alcance o sucesso de forma rápida e divertida. </h1>
+              <p>
+                A nossa plataforma permite que você defina metas, atribua tarefas e estabeleça prazos de maneira descomplicada. Imagine reunir sua equipe em um ambiente digital onde todos têm uma visão clara do que precisa ser feito.
+              </p>
+              <p>
+                Além disso, você pode monitorar o progresso em tempo real, identificando gargalos e oportunidades de otimização. Colabore de maneira eficaz, comentando diretamente nas tarefas e recebendo notificações instantâneas.
+              </p>
+            </div>
+            <img src={projeto} alt="" srcset="" />
+
           </div>
 
           <div id="tarefas" className='secao'>
-            <h1>A simplicidade do arrastar e soltar </h1>
-            <p>
-              Já imaginou ter total controle sobre o que precisa ser feito? Por quem e até quando?
-            </p>
-            <p>
-              Dentro da nossa plataforma, as tarefas são como peças de um quebra-cabeça que compõem o seu projeto. Elas podem ser criadas, atribuídas a membros da equipe e definidas com prazos claros.
-            </p>
+            <div className="texto">
+              <h1>A simplicidade do arrastar e soltar </h1>
+              <p>
+                Já imaginou ter total controle sobre o que precisa ser feito? Por quem e até quando?
+              </p>
+              <p>
+                Dentro da nossa plataforma, as tarefas são como peças de um quebra-cabeça que compõem o seu projeto. Elas podem ser criadas, atribuídas a membros da equipe e definidas com prazos claros.
+              </p>
+            </div>
+            <img src={projeto} alt="" srcset="" />
           </div>
+
           <div id="sobre" className='secao'>
             <h1>Sobre nós</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis animi non quis nihil suscipit pariatur, vero veniam ab tempora nobis blanditiis illum autem laborum quod exercitationem debitis laudantium. Distinctio, omnis?</p>
           </div>
+
         </div>
-        
-      {/* {<footer className="Wave">
-       
-      </footer> } */}
     </div>
     
   );
