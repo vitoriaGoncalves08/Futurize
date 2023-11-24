59e8e1884278ed272c4b9bf17352b3ca2837cf71
Vitória G <56117556+vitoriaGoncalves08@users.noreply.github.com>
2023-11-23 21:55:00 -0300

    :alien: alterando api rotas

--- futurize-frontend/src/routes/AppRoutes.jsx
+++ futurize-frontend/src/routes/AppRoutes.jsx
@@ -21,12 +21,12 @@ export default function AppRoutes() {
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<SiteInicial />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/app" element={<App />} />
+          <Route path="https://futurize-8kzp54a03-vitoriagoncalves08.vercel.app/" element={<SiteInicial />} />
+          <Route path="https://futurize-8kzp54a03-vitoriagoncalves08.vercel.app/cadastro" element={<Cadastro />} />
+          <Route path="https://futurize-8kzp54a03-vitoriagoncalves08.vercel.app/login" element={<Login />} />
+          <Route path="https://futurize-8kzp54a03-vitoriagoncalves08.vercel.app/app" element={<App />} />
           <Route
-            path="/projeto"
+            path="https://futurize-8kzp54a03-vitoriagoncalves08.vercel.app/projeto"
             element={
               <PrivateRoute>
                 <Projeto />
@@ -34,7 +34,7 @@ export default function AppRoutes() {
             }
           />
           <Route
-          path="/kanban/:projectId" 
+          path="https://futurize-8kzp54a03-vitoriagoncalves08.vercel.app/kanban/:projectId" 
           element={
             <PrivateRoute>
               <Kanban />
